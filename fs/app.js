59f1f2b30be99eaf50738afedc99e26dd5a00d2e
Maxim <zeetreex@gmail.com>
2024-09-02 11:14:02 +0300

    fs:refactor using event emitter

--- fs/app.js
+++ fs/app.js
@@ -1,21 +1,25 @@
 const fs = require("node:fs/promises");
 
 (async () => {
-	const file = await fs.open("./content.txt", "r")
+	const file = await fs.open("./content.txt", "r");
 	const watcher = fs.watch("./content.txt");
+
+	file.on("change", async () => {
+		const size = (await file.stat()).size;
+
+		const buffer = Buffer.alloc(size);
+		const offset = 0;
+		const length = buffer.byteLength;
+		const position = 0;
+
+		const fileContent = await file.read(buffer, offset, length, position);
+
+		console.log(fileContent);
+	});
 
 	for await (const event of watcher) {
 		if (event.eventType === "change") {
-			const size = (await file.stat()).size
-
-			const buffer = Buffer.alloc(size)
-			const offset = 0;
-			const length = buffer.byteLength;
-			const position = 0;
-
-			const fileContent = await file.read(buffer, offset, length, position);
-
-			console.log(fileContent);
+			file.emit("change");
 		}
 	}
 })();
