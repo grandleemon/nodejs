1a9da668449b6f350f8450504d5c5e6b5427a7d8
Maxim <zeetreex@gmail.com>
2024-09-02 14:31:55 +0300

    fs:creating a file using command

--- fs/app.js
+++ fs/app.js
@@ -1,6 +1,18 @@
 const fs = require("node:fs/promises");
 
 (async () => {
+	const createFile = async (path) => {
+		try {
+			await fs.access(path);
+			return console.log(`The file ${path} already exists`);
+		} catch (e) {
+			await fs.writeFile(path, "");
+			console.log("A new file was successfully created");
+		}
+	}
+
+	const CREATE_FILE = "create a file";
+
 	const file = await fs.open("./content.txt", "r");
 	const watcher = fs.watch("./content.txt");
 
@@ -14,7 +26,12 @@ const fs = require("node:fs/promises");
 
 		await file.read(buffer, offset, length, position);
 
-		console.log(buffer.toString("utf8"));
+		const command = buffer.toString("utf8");
+
+		if (command.includes(CREATE_FILE)) {
+			const filePath = command.substring(CREATE_FILE.length + 1);
+			await createFile(filePath)
+		}
 	});
 
 	for await (const event of watcher) {
