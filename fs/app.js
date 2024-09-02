43af0af6133bb1ea521c989a734ce10949107749
Maxim <zeetreex@gmail.com>
2024-09-02 11:06:25 +0300

    fs:reading the content of the file

--- fs/app.js
+++ fs/app.js
@@ -1,11 +1,21 @@
 const fs = require("node:fs/promises");
 
 (async () => {
+	const file = await fs.open("./content.txt", "r")
 	const watcher = fs.watch("./content.txt");
 
 	for await (const event of watcher) {
 		if (event.eventType === "change") {
-			console.log("The file was changed");
+			const size = (await file.stat()).size
+
+			const buffer = Buffer.alloc(size)
+			const offset = 0;
+			const length = buffer.byteLength;
+			const position = 0;
+
+			const fileContent = await file.read(buffer, offset, length, position);
+
+			console.log(fileContent);
 		}
 	}
 })();
