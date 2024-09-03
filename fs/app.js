32a62859df3b53322af04400fd07a10da3d28001
Maxim <zeetreex@gmail.com>
2024-09-03 11:06:49 +0300

    fs:deleting a file, renaming a file, adding to a file

--- fs/app.js
+++ fs/app.js
@@ -1,6 +1,11 @@
 const fs = require("node:fs/promises");
 
 (async () => {
+	const CREATE_FILE = "create a file";
+	const DELETE_FILE = "delete the file";
+	const RENAME_FILE = "rename the file";
+	const ADD_TO_FILE = "add to the file";
+
 	const createFile = async (path) => {
 		try {
 			await fs.access(path);
@@ -9,9 +14,36 @@ const fs = require("node:fs/promises");
 			await fs.writeFile(path, "");
 			console.log("A new file was successfully created");
 		}
-	}
+	};
 
-	const CREATE_FILE = "create a file";
+	const deleteFile = async (path) => {
+		try {
+			await fs.rm(path);
+			console.log(`The file ${path} was successfully deleted`);
+		} catch (e) {
+			console.log(`The file ${path} not found`);
+		}
+	};
+
+	const renameFile = async (path, newPath) => {
+		try {
+			await fs.rename(path, newPath);
+			console.log(`The file ${path} was successfully renamed`);
+		} catch (e) {
+			console.log(e);
+			console.log(`The file ${path} not found`);
+		}
+	};
+
+	const addToFile = async (path, content) => {
+		try {
+			await fs.appendFile(path, content);
+			console.log(`The content for ${path} was successfully added`);
+		} catch (e) {
+			console.log(e);
+			console.log(`The file ${path} not found`);
+		}
+	};
 
 	const file = await fs.open("./content.txt", "r");
 	const watcher = fs.watch("./content.txt");
@@ -30,7 +62,26 @@ const fs = require("node:fs/promises");
 
 		if (command.includes(CREATE_FILE)) {
 			const filePath = command.substring(CREATE_FILE.length + 1);
-			await createFile(filePath)
+			await createFile(filePath);
+		}
+
+		if (command.includes(DELETE_FILE)) {
+			const filePath = command.substring(DELETE_FILE.length + 1);
+			await deleteFile(filePath);
+		}
+
+		if (command.includes(RENAME_FILE)) {
+			const index = command.indexOf(" to ");
+			const oldFilePath = command.substring(RENAME_FILE.length + 1, index);
+			const newFilePath = command.substring(index + 4);
+			await renameFile(oldFilePath, newFilePath);
+		}
+
+		if (command.includes(ADD_TO_FILE)) {
+			const index = command.indexOf(" this content: ");
+			const filePath = command.substring(ADD_TO_FILE.length + 1, index);
+			const content = command.substring(index + 15);
+			await addToFile(filePath, content);
 		}
 	});
 
