d73d443e2856e90791d65bffd820a9cf3bbf9cf7
Maxim <zeetreex@gmail.com>
2024-09-06 12:49:10 +0300

    stream:super cool pipe/pipeline functions

--- stream/copy/copy.js
+++ stream/copy/copy.js
@@ -11,26 +11,48 @@ const fs = require("node:fs/promises");
 // 	await fs.writeFile(`copy-test.txt`, fileBuffer)
 // })()
 
-const CHUNK_SIZE = 16384;
+// const CHUNK_SIZE = 16384;
+//
+// (async () => {
+// 	const srcFile = await fs.open("test.txt", "r");
+// 	const destFile = await fs.open("test-copy.txt", "w");
+//
+// 	let bytesRead = -1;
+//
+// 	while (bytesRead !== 0) {
+// 		const readResult = await srcFile.read();
+// 		bytesRead = readResult.bytesRead;
+//
+// 		if (bytesRead !== CHUNK_SIZE) {
+// 			const indexOfNotFilled = readResult.buffer.indexOf(0);
+// 			const newBuffer = Buffer.alloc(indexOfNotFilled);
+// 			readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled);
+// 			await destFile.write(newBuffer);
+// 			continue;
+// 		}
+//
+// 		await destFile.write(readResult.buffer);
+// 	}
+// })();
+
+const { pipeline } = require("node:stream");
 
 (async () => {
+	console.time("Time");
 	const srcFile = await fs.open("test.txt", "r");
 	const destFile = await fs.open("test-copy.txt", "w");
 
-	let bytesRead = -1;
+	const readStream = srcFile.createReadStream();
+	const writeStream = destFile.createWriteStream();
 
-	while (bytesRead !== 0) {
-		const readResult = await srcFile.read();
-		bytesRead = readResult.bytesRead;
+	// readStream.pipe(writeStream);
+	//
+	// readStream.on("end", () => {
+	// 	console.timeEnd("Time");
+	// });
 
-		if (bytesRead !== CHUNK_SIZE) {
-			const indexOfNotFilled = readResult.buffer.indexOf(0);
-			const newBuffer = Buffer.alloc(indexOfNotFilled);
-			readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled);
-			await destFile.write(newBuffer);
-			continue;
-		}
-
-		await destFile.write(readResult.buffer);
-	}
+	pipeline(readStream, writeStream, (err) => {
+		console.log(err);
+		console.timeEnd("Time");
+	})
 })();
