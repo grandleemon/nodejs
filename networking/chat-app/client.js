7e9c20a108a1e44d062c7837e497aa692d79bd5a
Maxim <zeetreex@gmail.com>
2024-09-09 15:39:39 +0300

    networking:chat app sending information to server and then to all connected clients

--- networking/chat-app/client.js
+++ networking/chat-app/client.js
@@ -1,8 +1,26 @@
 const net = require("node:net");
+const readline = require("node:readline/promises");
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
 
 const client = net.createConnection({
 	port: 3000,
 	host: "127.0.0.1"
-}, () => {
+}, async () => {
 	console.log("Connect to the server");
+
+	const message = await rl.question("Enter a message: ");
+	client.write(message);
 });
+
+client.on("data", (data) => {
+	console.log(data.toString("utf8"));
+});
+
+client.on("end", () => {
+	console.log("Connection was ended");
+});
+
--- networking/chat-app/server.js
+++ networking/chat-app/server.js
@@ -2,9 +2,19 @@ const net = require("node:net");
 
 const server = net.createServer();
 
+const clients = [];
+
 server.on("connection", (socket) => {
 	console.log("A new connection to the server");
-})
+
+	clients.push(socket);
+
+	socket.on("data", (data) => {
+		clients.forEach(socket => {
+			socket.write(data);
+		})
+	});
+});
 
 server.listen(3000, "127.0.0.1", () => {
 	console.log("Opened server on: ", server.address());
