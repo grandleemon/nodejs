73ade21d4abadb2bf5a295e34b1dd3e16255f9e6
Maxim <zeetreex@gmail.com>
2024-09-09 16:12:49 +0300

    networking:chat app fix ui

--- networking/chat-app/client.js
+++ networking/chat-app/client.js
@@ -6,19 +6,48 @@ const rl = readline.createInterface({
 	output: process.stdout
 });
 
+const clearLine = (dir) => {
+	return new Promise((res, rej) => {
+		process.stdout.clearLine(dir, () => {
+			res();
+		});
+	});
+};
+
+const moveCursor = (dx, dy) => {
+	return new Promise((res, rej) => {
+		process.stdout.moveCursor(dx, dy, () => {
+			res();
+		});
+	});
+};
+
 const client = net.createConnection({
 	port: 3000,
 	host: "127.0.0.1"
 }, async () => {
 	console.log("Connect to the server");
 
-	const message = await rl.question("Enter a message: ");
-	client.write(message);
+	const ask = async () => {
+		const message = await rl.question("Enter a message: ");
+		await moveCursor(0, -1);
+		await clearLine(0);
+		client.write(message);
+	};
+
+	await ask();
+
+	client.on("data", async (data) => {
+		console.log();
+		await moveCursor(0, -1);
+		await clearLine(0);
+		console.log(data.toString("utf8"));
+
+		await ask();
+	});
 });
 
-client.on("data", (data) => {
-	console.log(data.toString("utf8"));
-});
+
 
 client.on("end", () => {
 	console.log("Connection was ended");
